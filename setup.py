b0558da0fcc6f0b3adf42b596599187ec282abd8
unternehmen <unternehmen@users.noreply.github.com>
2018-05-13 13:33:56 -0400

    Add license and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='dirt',
     description='A first-person, turn-based adventure game',
     version='0.1.0',
+    license='GPLv3',
     packages=find_packages(),
     install_requires=['pygame==1.9.3'],
     include_package_data=True,
@@ -11,5 +12,15 @@ setup(
         'console_scripts': [
             'dirt = dirt:main'
         ]
-    }
+    },
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Games/Entertainment :: Role-Playing'
+    ]
 )
